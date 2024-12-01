5313226065f65b26f169e8f8a277bd3f080adb33
Jacob Greer <jacobmgreer@users.noreply.github.com>
2024-11-30 19:29:35 -0800

    Update nightly.R

--- R/nightly.R
+++ R/nightly.R
@@ -11,8 +11,8 @@ TMAPI <- Sys.getenv("TMAPI") ## API key defined in Repo
 
 source("R/steps/tm-content.R")
 source("R/steps/tm-people.R")
-# source("R/steps/wd-albums.R")
-# source("R/steps/wd-films.R")
-# source("R/steps/wd-people.R")
+source("R/steps/wd-albums.R")
+source("R/steps/wd-films.R")
+source("R/steps/wd-people.R")
 
 unlink("updates", recursive = T, force = T)
