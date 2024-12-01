342bf851cdee3a109ab031498d0488c690659589
Jacob Greer <jacobmgreer@users.noreply.github.com>
2024-11-30 19:16:25 -0800

    updates

--- R/nightly.R
+++ R/nightly.R
@@ -1,8 +1,6 @@
-required <- c("tidyverse", "jsonlite", "arrow")
-lapply(required, require, character.only = TRUE)
+lapply(c("tidyverse", "jsonlite", "arrow"), require, character.only = TRUE)
 options(readr.show_col_types = FALSE)
 options(timeout = 10*60)
-rm(required)
 
 dir.create("nightlies", showWarnings = F)
 dir.create("updates", showWarnings = F)
@@ -13,8 +11,8 @@ TMAPI <- Sys.getenv("TMAPI") ## API key defined in Repo
 
 source("R/steps/tm-content.R")
 source("R/steps/tm-people.R")
-source("R/steps/wd-albums.R")
-source("R/steps/wd-films.R")
-source("R/steps/wd-people.R")
+# source("R/steps/wd-albums.R")
+# source("R/steps/wd-films.R")
+# source("R/steps/wd-people.R")
 
 unlink("updates", recursive = T, force = T)
