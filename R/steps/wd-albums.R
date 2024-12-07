8aecb7f34046462baee64665400a220aa2f41570
Jacob Greer <jacobmgreer@gmail.com>
2024-12-07 02:31:35 +0000

    Update Nitration Nightly

---
+++ R/steps/wd-albums.R
@@ -0,0 +1,16 @@
+## WD Albums
+dir.create("updates/albums", showWarnings = F)
+
+query <- URLencode(str_squish(str_replace_all(read_file("SPARQL/wd-albums.sparql"), "[\r\n]" , " ")))
+download.file(
+  url = paste0("https://query.wikidata.org/sparql?query=", query, "&format=json"),
+  destfile = "updates/albums/query.json")
+query_results <- 
+  jsonlite::fromJSON("updates/albums/query.json")$results$bindings %>%
+  reframe(
+    QID = basename(item$value),
+    updated = substr(date$value, 1, 10),
+    file = paste0(updated, "-", QID, ".rds"))
+
+dir.create("nightlies/qid-album", showWarnings = F)
+write_parquet(query_results, "nightlies/qid-album/nightly.parquet")
